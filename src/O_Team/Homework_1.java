b8826c2e43aaecf969cdd06059a226ccf59dc5d5
davidAllenStephan <david.marino03@hotmail.com>
2024-01-29 17:00:46 -0600

    Changed Davids console print

--- src/O_Team/Homework_1.java
+++ src/O_Team/Homework_1.java
@@ -5,7 +5,8 @@ public class Homework_1 {
 	public static void main (String[] args) {
 		//Ashley added this
 		System.out.println("Name: Ashley O'Gorman, Git ID: ashleyogorman");
-		System.out.println("davidAllenStepan");
+    //David added this
+		System.out.println("Name: David Marino, Git ID: davidAllenStephan");
 		System.out.println("ishavers");
 		//Sydney added this
 		System.out.println("Name: Sydney Poulsen, Git ID: sydpoulsen");
