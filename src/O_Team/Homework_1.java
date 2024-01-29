8a029dff367d7046a1d973ac20d657e086957e28
davidAllenStephan <david.marino03@hotmail.com>
2024-01-29 15:08:55 -0600

    initial commit

---
+++ src/O_Team/Homework_1.java
@@ -0,0 +1,11 @@
+package O_Team;
+
+public class Homework_1 {
+
+	public static void main (String[] args) {
+		System.out.println("ashleyogorman");
+		System.out.println("davidAllenStepan");
+		System.out.println("ishavers");
+		System.out.println("sydpoulsen");
+	}
+}
