ee9418db95ef8113abe7f62c9f583efb912f5639
Ashley OGorman <ashley87105@gmail.com>
2024-01-29 16:22:17 -0600

    added my name

--- src/O_Team/Homework_1.java
+++ src/O_Team/Homework_1.java
@@ -3,7 +3,8 @@ package O_Team;
 public class Homework_1 {
 
 	public static void main (String[] args) {
-		System.out.println("ashleyogorman");
+		//Ashley added this
+		System.out.println("Name: Ashley O'Gorman, Git ID: ashleyogorman");
 		System.out.println("davidAllenStepan");
 		System.out.println("ishavers");
 		//Sydney added this
