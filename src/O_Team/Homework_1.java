11be5179b7f4e136d5923a1071385989747147d7
Isaiah Shavers <112894709+ishavers@users.noreply.github.com>
2024-01-31 15:22:01 -0600

    Isaiah

--- src/O_Team/Homework_1.java
+++ src/O_Team/Homework_1.java
@@ -7,7 +7,7 @@ public class Homework_1 {
 		System.out.println("Name: Ashley O'Gorman, Git ID: ashleyogorman");
     //David added this
 		System.out.println("Name: David Marino, Git ID: davidAllenStephan");
-		System.out.println("ishavers");
+		System.out.println("Name: Isaiah Shavers, Git ID: ishavers");
 		//Sydney added this
 		System.out.println("Name: Sydney Poulsen, Git ID: sydpoulsen");
 	}
